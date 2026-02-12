1ae882bf4b256497b8d40ba6bdbcb6c27807193a
SasidharRegula <sasidharregula22@gmail.com>
2026-02-12 16:49:56 +0530

    made parameter change

--- function_app.py
+++ function_app.py
@@ -9,9 +9,9 @@ from azure.cosmos import CosmosClient
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
-@app.route(route="emailcomplaintsgithub")
+# app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app=func.FunctionApp()
+@app.route(route="emailcomplaintsgithub", auth_level=func.AuthLevel.ANONYMOUS)
 def emailcomplaintsgithub(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     BLOB_CONN_STR=os.getenv("BLOB_CONN_STR") 
